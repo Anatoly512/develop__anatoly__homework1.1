13793262ad7f0c37000b6242e086835266d29ee2
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2020-05-19 19:09:02 +0300

    add .sql files

---
+++ initDB___anatoly.sql
@@ -0,0 +1,148 @@
+-- PostgreSQL version: 12.0
+
+
+CREATE TABLE developers (
+	id serial NOT NULL,
+	name character varying(200),
+	gender int2,
+	age int2,
+	CONSTRAINT id1 PRIMARY KEY (id)
+
+);
+
+CREATE TABLE projects (
+	id serial NOT NULL,
+	projects_name character varying(200),
+	CONSTRAINT id2 PRIMARY KEY (id)
+
+);
+
+CREATE TABLE companies (
+	id serial NOT NULL,
+	company_name character varying(200),
+	CONSTRAINT id3 PRIMARY KEY (id)
+
+);
+
+CREATE TABLE customers (
+	id serial NOT NULL,
+	name character varying(200),
+	CONSTRAINT id4 PRIMARY KEY (id)
+
+);
+
+CREATE TABLE skills (
+	id serial NOT NULL,
+	language character varying(200),
+	level int2,
+	CONSTRAINT id5 PRIMARY KEY (id)
+
+);
+
+CREATE TABLE customers_projects (
+	id serial NOT NULL,
+	projects_id serial,
+	customers_id serial,
+	id_projects integer,
+	id_customers integer,
+	CONSTRAINT id6 PRIMARY KEY (id)
+
+);
+
+ALTER TABLE customers_projects ADD CONSTRAINT projects_fk FOREIGN KEY (id_projects)
+REFERENCES projects (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE customers_projects ADD CONSTRAINT customers_fk FOREIGN KEY (id_customers)
+REFERENCES customers (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+CREATE TABLE companies_projects (
+	id serial NOT NULL,
+	projects_id serial,
+	companies_id serial,
+	id_projects integer,
+	id_companies integer,
+	CONSTRAINT id7 PRIMARY KEY (id)
+
+);
+
+ALTER TABLE companies_projects ADD CONSTRAINT projects_fk FOREIGN KEY (id_projects)
+REFERENCES projects (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE companies_projects ADD CONSTRAINT companies_fk FOREIGN KEY (id_companies)
+REFERENCES companies (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+CREATE TABLE developers_skills (
+	id serial NOT NULL,
+	developers_id serial,
+	skills_id serial,
+	id_skills integer,
+	id_developers integer,
+	CONSTRAINT id8 PRIMARY KEY (id)
+
+);
+
+CREATE TABLE developers_projects (
+	id serial NOT NULL,
+	projects_id serial,
+	developers_id serial,
+	id_projects integer,
+	id_developers integer,
+	CONSTRAINT id9 PRIMARY KEY (id)
+
+);
+
+ALTER TABLE developers_projects ADD CONSTRAINT projects_fk FOREIGN KEY (id_projects)
+REFERENCES projects (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE developers_projects ADD CONSTRAINT developers_fk FOREIGN KEY (id_developers)
+REFERENCES developers (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE developers_skills ADD CONSTRAINT skills_fk FOREIGN KEY (id_skills)
+REFERENCES skills (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE developers_skills ADD CONSTRAINT developers_fk FOREIGN KEY (id_developers)
+REFERENCES developers (id) MATCH FULL
+ON DELETE SET NULL ON UPDATE CASCADE;
+
+ALTER TABLE customers_projects ADD CONSTRAINT projects_id FOREIGN KEY (projects_id)
+REFERENCES projects (id) MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE customers_projects ADD CONSTRAINT customers_id FOREIGN KEY (customers_id)
+REFERENCES customers (id) MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE companies_projects ADD CONSTRAINT projects_id FOREIGN KEY (projects_id)
+REFERENCES projects (id) MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE companies_projects ADD CONSTRAINT companies_id FOREIGN KEY (companies_id)
+REFERENCES companies (id) MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE developers_skills ADD CONSTRAINT developers_id FOREIGN KEY (developers_id)
+REFERENCES developers (id) MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE developers_skills ADD CONSTRAINT skills_id FOREIGN KEY (skills_id)
+REFERENCES skills (id) MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE developers_projects ADD CONSTRAINT projects_id FOREIGN KEY (projects_id)
+REFERENCES projects (id) MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE developers_projects ADD CONSTRAINT developers_id FOREIGN KEY (developers_id)
+REFERENCES developers (id) MATCH FULL
+ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- end --
+
+
---
+++ populateDB___anatoly.sql
@@ -0,0 +1,48 @@
+
+-- where gender 1 - male, 2 - female
+
+ INSERT INTO developers (name, gender, age) VALUES ('Tom', 1, 32);
+ INSERT INTO developers (name, gender, age) VALUES ('Jerry', 1, 18);
+ INSERT INTO developers (name, gender, age) VALUES ('Vadim', 1, 26);
+ INSERT INTO developers (name, gender, age) VALUES ('Misha', 1, 39);
+ INSERT INTO developers (name, gender, age) VALUES ('Dasha', 2, 28);
+
+-- where level 1 - junior, 2 - middle, 3 - senior
+
+ INSERT INTO skills (id, language, level) VALUES (1, 'Java', 2);
+ INSERT INTO skills (id, language, level) VALUES (2, 'JS', 1);
+ INSERT INTO skills (id, language, level) VALUES (3, 'C++', 2);
+ INSERT INTO skills (id, language, level) VALUES (4, 'Java', 3);
+ INSERT INTO skills (id, language, level) VALUES (5, 'C#', 2);
+
+
+ INSERT INTO projects (id, projects_name) VALUES (1, 'SQL application');
+ INSERT INTO projects (id, projects_name) VALUES (2, 'Mobile App');
+ INSERT INTO projects (id, projects_name) VALUES (3, 'Tools Inventory');
+ INSERT INTO projects (id, projects_name) VALUES (4, 'Support APS');
+ INSERT INTO projects (id, projects_name) VALUES (5, 'Calculate expenses');
+
+
+ INSERT INTO companies (id, company_name) VALUES (1, 'APS Technologies');
+ INSERT INTO companies (id, company_name) VALUES (2, 'Google Corp.');
+ INSERT INTO companies (id, company_name) VALUES (3, 'Intel');
+ INSERT INTO companies (id, company_name) VALUES (4, 'Microsoft');
+ INSERT INTO companies (id, company_name) VALUES (5, 'Roga&Copita investigations');
+
+
+ INSERT INTO customers (id, name) VALUES (1, 'Nikolay Valentinovich');
+ INSERT INTO customers (id, name) VALUES (2, 'Igor Maximovich Dimchenko');
+ INSERT INTO customers (id, name) VALUES (3, 'Andrey Mikholaenko');
+ INSERT INTO customers (id, name) VALUES (4, 'Evgeniy Sinitzin');
+ INSERT INTO customers (id, name) VALUES (5, 'Bogdan Andreevich Neizhenko');
+
+
+
+--SELECT * FROM developers;
+--SELECT * FROM skills;
+--SELECT * FROM projects
+--SELECT * FROM companies
+--SELECT * FROM customers
+
+
+
